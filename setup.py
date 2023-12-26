955a9a393042693b37da0559dd608dc80187d5db
Robert Tene <tenerobert200@yahoo.com>
2023-12-26 20:49:19 +0200

    Added a orders page to further expand the database functionality of all vm provisioning orders

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RobertAzureAPI',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='roberttene',
+    author_email='',
+    description=''
+)
